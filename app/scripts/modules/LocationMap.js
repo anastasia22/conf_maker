c53de61d4a3f3739d07b221011ec6c8dde65c437
anastasia22 <abuniak22@gmail.com>
2015-03-27 18:00:42 +0200

    started refactoring with location map

--- app/scripts/modules/LocationMap.js
+++ app/scripts/modules/LocationMap.js
@@ -4,53 +4,27 @@ var utilities = require('../utilities');
 var config = require('../config');
 
 var LocationMap = React.createClass({
+  getInitialState: function() {
+    return ({
+      header: ''
+    });
+  },
   componentWillMount: function() {
-    google.maps.event.addDomListener(window, 'load', this.initialize);
     utilities.ajax('get', config.path.location, function(data) {
       var temp = JSON.parse(data);
       this.setState({
-        header: temp.title,
-        linkTitle: temp.linkTitle
+        header: temp.title
       });
     }.bind(this));
-  },
-  initialize: function() {
-    var myLatlng = new google.maps.LatLng(49.842721, 24.000630);
-    var mapProp = {
-      center: myLatlng,
-      zoom: 17,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      scrollwheel: false,
-      draggable: false
-    };
-    var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Awesome place for conference! See you there!'
-    });
-    //creating custom control - link to maps website
-    var controlDiv = document.createElement('div');
-    controlDiv.className = 'location__control'
-    var controlText = document.createElement('a');
-    controlText.className = 'location__controlText';
-    controlText.href = 'http://maps.google.com/maps?&z=' + mapProp.zoom + '&q=' + myLatlng.k + ',' + myLatlng.D;
-    controlText.target = 'blank';
-    controlText.innerHTML = this.state.linkTitle;
-    controlDiv.appendChild(controlText);
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(controlDiv);
-  },
-  getInitialState: function() {
-    return ({
-      header: '',
-      linkTitle: ''
-    });
   },
   render: function() {
     return (
       <section id="location" className="page-wrap">
         <h2 className="module-header">{this.state.header}</h2>
-        <div id="googleMap" className="location">
+        <div className="location">
+          <iframe height="500" 
+          src="https://www.google.com/maps/embed/v1/place?q=45%20%D0%B2%D1%83%D0%BB%D0%B8%D1%86%D1%8F%20%D0%9E%D0%BB%D0%B5%D0%BD%D0%B8%20%D0%A1%D1%82%D0%B5%D0%BF%D0%B0%D0%BD%D1%96%D0%B2%D0%BD%D0%B8%2C%20%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%2C%20%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C%20%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0&key=AIzaSyDbHy0WuueUyg10oJeFzz_qPpCb1yuqZ20&center=">
+          </iframe>
         </div>
       </section>
     );
